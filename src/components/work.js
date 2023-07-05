74c3797f8305ebcbb991d1ba0ca42eb49da85e03
ru201 <rubinroy12@gmail.com>
2023-07-05 15:37:23 +1000

    began some animation

--- src/components/work.js
+++ src/components/work.js
@@ -11,7 +11,7 @@ export default function Work () {
         <div className="homeMsg">
             <h1 className="h1Msg">My Work</h1>
             <h2 className="h2Msg">Check out some of my personal projects on GitHub.</h2>
-            <a onclick="" href="https://www.github.com/ru201" rel="noreferrer" target='_blank'><FontAwesomeIcon onclick="" icon={faGithub} className="social_icon gh" /></a>
+            <a onclick="" href="https://www.github.com/ru201" rel="noreferrer" target='_blank'><FontAwesomeIcon icon={faGithub} className="social_icon gh" /></a>
         </div>
         <div className="portfolioPic">
             <Img src={Pic} alt={"Picture of Me"} className={"pic"} />
